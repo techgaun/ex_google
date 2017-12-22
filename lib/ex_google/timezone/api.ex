e56f82cd025291ce3fa8284a44400f715782e84b
jonathanhockman <jonathan.hockman@gmail.com>
2017-12-21 18:44:04 -0600

    Add timezone

---
+++ lib/ex_google/timezone/api.ex
@@ -0,0 +1,46 @@
+defmodule ExGoogle.Timezone.Api do
+  @moduledoc """
+  Simple API wrapper for google maps
+  """
+  use HTTPoison.Base
+  alias __MODULE__
+  alias ExGoogle.Parser
+  import ExGoogle.Utils
+
+  @base_url "https://maps.googleapis.com/maps/api/timezone"
+
+  @doc """
+  Use google maps api to perform geocoding and reverse geocoding
+
+  The first params map can consist of any field that google map supports.
+  While the examples below show few parameters, passing any parameters that google maps api supports works.
+
+  * [Google Maps API](https://developers.google.com/maps/documentation/timezone/intro)
+
+  ## Examples
+
+      alias ExGoogle.Maps.Api, as: Maps
+      # google maps reverse geocoding
+      Maps.search(%{latlng: "40.714224,-73.961452"})
+
+      # google maps geocoding
+      Maps.search(%{address: "1600 Amphitheatre Parkway, Mountain View, CA"})
+
+      # Advanced parameters
+      Maps.search(%{latlng: "40.714224,-73.961452", location_type: "ROOFTOP|RANGE_INTERPOLATED|GEOMETRIC_CENTER", result_type: "street_address"})
+  """
+  @spec search(map) :: {:ok, binary} | {:error, binary}
+  def search(params) when is_map(params) and map_size(params) > 0 do
+    params
+    |> Map.put(:key, api_key())
+    |> build_url
+    |> Api.get(request_headers())
+    |> Parser.parse
+  end
+  def search(_), do: {:error, "invalid request"}
+
+  @spec build_url(map) :: String.t
+  def build_url(params) do
+    "#{@base_url}/#{output()}?#{URI.encode_query(params)}"
+  end
+end
