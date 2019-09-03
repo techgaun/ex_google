df740781675613ba4d0ccbcf261663947fb92e5a
techgaun <coolsamar207@gmail.com>
2019-09-03 00:27:51 -0500

    fix docs

--- lib/ex_google.ex
+++ lib/ex_google.ex
@@ -1,8 +1,15 @@
 defmodule ExGoogle do
   @moduledoc """
   ExGoogle is an Elixir client for google cloud APIs.
-  ExGoogle is a work in progress. Currently, it supports google places and google maps geocoding apis and json output format.
 
-  Please check individual Places and Maps modules for more details on how to use.
+  ExGoogle is a work in progress.
+  Currently, it supports:
+  - google places
+  - google maps geocoding apis
+  - google timezone apis
+
+  As for format, it supports json output format by default.
+
+  Please check individual Maps, Places and Timezone modules for more details on how to use.
   """
 end
