9c69dfaa67bba61fa2d48101d2609520263aee8f
techgaun <coolsamar207@gmail.com>
2019-09-02 23:36:24 -0500

    upgrade deps

--- mix.exs
+++ mix.exs
@@ -34,8 +34,8 @@ defmodule ExGoogle.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:httpoison, "~> 1.2"},
-      {:poison, "~> 3.1.0", override: true},
+      {:httpoison, ">= 1.2.0"},
+      {:poison, ">= 3.1.0"},
       {:earmark, "~> 0.1", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev},
       {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
